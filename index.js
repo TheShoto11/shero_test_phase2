eb3db20b9c4c6ca2627423511bb9e84593e75dae
Arlind <arlindi.shehi@hotmail.com>
2025-01-23 10:16:19 +0100

    removed breakpoints since its task 3

--- index.js
+++ index.js
@@ -3,9 +3,5 @@ document.addEventListener("DOMContentLoaded", function () {
     type: "carousel",
     perView: 4,
     gap: 10,
-    breakpoints: {
-      768: { perView: 2 }, // Adjust for tablet
-      480: { perView: 1 }, // Adjust for mobile
-    },
   }).mount()
 })
